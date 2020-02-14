9931988f25cf12742f8be0825f307f81051eb89e
Toru_Nakandakari <arusu0629@gmail.com>
2020-02-14 22:18:14 +0900

    add initial project

---
+++ CurrencyConversionApp/CurrencyConversionApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CurrencyConversionApp
+//
+//  Created by Toru Nakandakari on 2020/02/14.
+//  Copyright © 2020 仲村渠亨. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
